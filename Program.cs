46e6b0bd6815478a09aeb28d30ede5e67ef7622c
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-12 11:19:15 +0000

    Initialised with delegates example

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Delegate handler to call a method
+            UsingDelegates.MyDelegate myDelegateHandler = UsingDelegates.DelegateMethod;
+            myDelegateHandler("Hello World");
+            UsingDelegates.MethodWithCallback(20, 30, myDelegateHandler);
+
+            // Using a single delegate to call other delegates to call multiple methods (multicasting)
+            UsingDelegates.MyDelegate myDelegateHandler2 = UsingDelegates.DelegateMethod2;
+            UsingDelegates.MyDelegate myGroupedDelegateHandler = myDelegateHandler + myDelegateHandler2;
+            myGroupedDelegateHandler("Hello Bob");
+
+            // Removing a delegate from the grouped delegate handler
+            myGroupedDelegateHandler -= myDelegateHandler;
+            myGroupedDelegateHandler("Hello Fred");
+        }
+    }
+}
---
+++ UsingDelegates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public class UsingDelegates
+    {
+        // This is the delegate type 'MyDelegate'
+        public delegate void MyDelegate(string message);
+
+        // This is the method that will be delegated to from 'MyDelegate'
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void DelegateMethod2(string message)
+        {
+            Console.WriteLine(message.ToUpper());
+        }
+
+        public static void MethodWithCallback(int num1, int num2, MyDelegate callback)
+        {
+            callback($"The number is: {num1 + num2}");
+        }
+    }
+}
